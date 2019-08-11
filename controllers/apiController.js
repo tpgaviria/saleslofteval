689f30e00ff7606383b90c7adb24004dc983c030
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-08-11 14:04:32 -0400

    gitignore added

---
+++ controllers/apiController.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+require('dotenv').config();
+
+module.exports = {
+    getPeopleData: function(req, res) {
+        const KEY = process.env.API_KEY;
+        
+    }
+}
