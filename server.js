d5607907906c72287728e2b421a443f55a21549c
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-08-11 18:12:58 -0400

    deployment config

--- server.js
+++ server.js
@@ -15,4 +15,8 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 app.listen(PORT, () => console.log(`Server: ${PORT}`));
